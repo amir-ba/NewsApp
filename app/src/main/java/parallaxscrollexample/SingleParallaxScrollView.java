1f779db763b824caa157f37b9e68e334481d1434
Meshec <murcko.juraj@gmail.com>
2016-01-19 18:46:32 +0100

    Added About page

--- app/src/main/java/parallaxscrollexample/SingleParallaxScrollView.java
+++ app/src/main/java/parallaxscrollexample/SingleParallaxScrollView.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 
 import com.example.s1908114.newsapp.R;
 
-import NewsApp.fragment.MapArticleActivity;
 
 public class SingleParallaxScrollView extends Activity {
 
@@ -46,14 +45,14 @@ public class SingleParallaxScrollView extends Activity {
 
     private View.OnClickListener startListener = new View.OnClickListener() {
         public void onClick(View v) {
-            Intent intent1 = new Intent(getApplicationContext(), MapArticleActivity.class);
+     //       Intent intent1 = new Intent(getApplicationContext(), MapArticleActivity.class);
 
             Intent intent = getIntent();
-            intent1.putExtra("lat", intent.getStringExtra("lat"));
-            intent1.putExtra("lon", intent.getStringExtra("lon"));
+          //  intent1.putExtra("lat", intent.getStringExtra("lat"));
+          //  intent1.putExtra("lon", intent.getStringExtra("lon"));
         String lon = intent.getStringExtra("lon");
         //Toast.makeText( getApplicationContext(), lat, Toast.LENGTH_SHORT).show();
-            startActivity(intent1);
+         //   startActivity(intent1);
 
 
 
